502cee46b5c1a6497026455c32c34be05b032d66
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-10-14 07:22:24 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "assignmentW1D4"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    println("Hello World!")
+
+
+}
+
+
+//we use this type of function when we dont return value to the main ,in this example we use function to print
+
+fun hello(){
+
+    println("HELLO!")
+}
